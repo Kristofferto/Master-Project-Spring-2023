733f4781bbba8fa0a65944d84b671c76a9ab3d88
Kristoffer Falk Austnes <kfaustnes@gmail.com>
2023-02-16 12:50:33 +0100

    Update test.py

--- test.py
+++ test.py
@@ -4,6 +4,10 @@ from tqdm import trange
 
 a= np.random.randint(0,4,20)
 
+path = 'C:/Users/krisfau/Desktop/VSCode/Data/'
+
+
+PCP = np.load(f"{path}Data_A_PCP_flag.npy", allow_pickle = True)
 
 a = np.array([0,0,0,0,0,0,1,1,4,4,4,1,1,0,0,0,1,1,1,4,4,4,1,1,1,0,0,0])
 print(a)
@@ -14,7 +18,7 @@ print(np.where(a[:-1] == 0)[0])
 
 print('###########')
 #MEN IKKE DENNE.... POKKER HELLER
-print(np.where((a[:-1] == 0) & (a[:-1] != a[1:]))[0])
+print(np.where((PCP[:-1] == 0) & (PCP[:-1] != PCP[1:]))[0])
 
     # print(np.where(a == 0)[0])
 
