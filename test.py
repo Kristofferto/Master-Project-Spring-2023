b0a6ef8da49fe9338af61b30025fbeba8d24b134
Kristoffer Falk Austnes <kfaustnes@gmail.com>
2023-02-16 12:39:07 +0100

    Update test.py

--- test.py
+++ test.py
@@ -5,17 +5,17 @@ from tqdm import trange
 a= np.random.randint(0,4,20)
 
 
-a = np.array([0,0,0,0,0,0,1,1,1,1,1,1,4,4,4,4,4,4,4,4,4,4,4,1,1,1,1,1,0,0,0,0,0,0,1,1,1,4,4,4,1,1,1,0,0,0])
+a = np.array([0,0,0,0,0,0,1,1,4,4,4,1,1,0,0,0,1,1,1,4,4,4,1,1,1,0,0,0])
 print(a)
-
+print(len(a))
 #hvorfor funker disse alene
 print(np.where((a[:-1] != a[1:]))[0])
-print(np.where(a == 0)[0])
+print(np.where(a[:-1] == 0)[0])
 
 print('###########')
 #MEN IKKE DENNE.... POKKER HELLER
-print(np.where((a[:-1] == 0) & (a[:-1] != a[1:]) & (a[1:] != a[:-1]))[0])
+print(np.where((a[:-1] == 0) & (a[:-1] != a[1:]))[0])
 
-    # print(np.where(a == 0)[0]) hei
+    # print(np.where(a == 0)[0])
 
 
