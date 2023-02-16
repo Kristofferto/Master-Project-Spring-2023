c813aeb4dff10e0226c56324a9120e6c871169a4
Kristoffer Falk Austnes <kfaustnes@gmail.com>
2023-02-16 16:57:28 +0100

    Update test.py

--- test.py
+++ test.py
@@ -11,9 +11,21 @@ print(a)
 #hvorfor funker disse alene
 print(np.where((a[:-1] != a[1:]))[0])
 print(np.where(a == 0)[0])
-
+print(a[:-1])
+print(a[1:])
 print('###########')
 #MEN IKKE DENNE.... POKKER HELLER
-print(np.where((a[:-1] == 0) & (a[:-1] != a[1:]) & (a[1:] != a[:-1]))[0])
+# print(np.where((a[:-1] == 0) & (a[:-1] != a[1:]) & (a[1:] != a[:-1]))[0])
 #test 123456kjhfwekfhwefkh
     # print(np.where(a == 0)[0]) hei
+
+#DETTE FUNGERER FAKTISK
+print(np.where((a == 0) & (a != np.roll(a,1)))[0])
+print(np.where((a == 0) & (a != np.roll(a,-1)))[0])
+
+en = np.where((a == 0) & (a != np.roll(a,1)))[0]
+to = np.where((a == 0) & (a != np.roll(a,-1)))[0]
+
+array = np.concatenate((en, to))
+array1 = np.sort(array)
+print(array1)
