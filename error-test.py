cf9f3d57e92ef2fe30fb6e77ba875c4e2123029f
Kristoffer Falk Austnes <kfaustnes@gmail.com>
2023-02-28 14:48:59 +0100

    Update error-test.py

--- error-test.py
+++ error-test.py
@@ -70,31 +70,61 @@ print(f'### Currently saving the data for satellite {sat} ###')
 
 
 
-data_list = ['MLT']
+# data_list = ['MLT']
+# sat_list = ['A', 'B', 'C']
+
+
+# path = 'C:/Users/krisfau/Desktop/VSCode/Data/'
+
+# for sat in sat_list:
+#     for name in data_list:
+#         exec(f'{name}_{sat} = np.load("{path}Data_{sat}_{name}.npy", allow_pickle = True)')
+
+# count_A = 0
+# count_B = 0
+# count_C = 0
+# for i in trange(len(MLT_A)):
+#     if np.isnan(MLT_A[i]):
+#         count_A +=1
+
+# for i in trange(len(MLT_B)):
+#     if np.isnan(MLT_B[i]):
+#         count_B +=1
+
+# for i in trange(len(MLT_C)):
+#     if np.isnan(MLT_C[i]):
+#         count_C +=1
+
+# print('A:', len(MLT_A) - count_A)
+# print('B:', len(MLT_B) - count_B)
+# print('C:', len(MLT_C) - count_C)
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+from tqdm import tqdm, trange
+# data_list = ['Background_Ne', 'Foreground_Ne', 'Grad_Ne_at_100km', 'Grad_Ne_at_20km', 'Grad_Ne_at_50km', 'Grad_Ne_at_PCP_edge',  'Longitude', 'Ne', 'PCP_flag', 'Radius', 'Timestamp', 'Latitude', 'MLT']
+data_list = ['Ne', 'PCP_flag', 'Timestamp', 'MLT', 'MLAT', 'Background_Ne']
 sat_list = ['A', 'B', 'C']
 
+#path mac
+# path = 'Data/'
+# #path hjemme
+# path = 'D:\\Git_Codes\\Data\\'
 
+# #path UiO
 path = 'C:/Users/krisfau/Desktop/VSCode/Data/'
 
-for sat in sat_list:
+
+for sat in tqdm(sat_list, desc = 'Loading satellite data'):
     for name in data_list:
         exec(f'{name}_{sat} = np.load("{path}Data_{sat}_{name}.npy", allow_pickle = True)')
 
-count_A = 0
-count_B = 0
-count_C = 0
-for i in trange(len(MLT_A)):
-    if np.isnan(MLT_A[i]):
-        count_A +=1
+for sat in sat_list:
+    exec(f'change01 = np.where((PCP_flag_{sat} == 0) & (PCP_flag_{sat} != np.roll(PCP_flag_{sat},1)))[0]')
+    exec(f'change10 = np.where((PCP_flag_{sat} == 0) & (PCP_flag_{sat} != np.roll(PCP_flag_{sat},-1)))[0]')
+    PCP_indices = np.concatenate((change01, change10))
+    exec(f'PCP_index_{sat} = np.sort(PCP_indices)')
 
-for i in trange(len(MLT_B)):
-    if np.isnan(MLT_B[i]):
-        count_B +=1
-
-for i in trange(len(MLT_C)):
-    if np.isnan(MLT_C[i]):
-        count_C +=1
-
-print('A:', len(MLT_A) - count_A)
-print('B:', len(MLT_B) - count_B)
-print('C:', len(MLT_C) - count_C)
+print(PCP_flag_B[:200])
+print(PCP_index_B[:100])
