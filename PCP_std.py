62f94afb0b9eb163b89fc32624007f605c5b04c3
Kristoffer Falk Austnes <kfaustnes@gmail.com>
2023-03-08 10:58:43 +0100

    Update PCP_std.py

--- PCP_std.py
+++ PCP_std.py
@@ -5,7 +5,7 @@ from tqdm import tqdm, trange
 import sys
 from scipy import stats
 # data_list = ['Background_Ne', 'Foreground_Ne', 'Grad_Ne_at_100km', 'Grad_Ne_at_20km', 'Grad_Ne_at_50km', 'Grad_Ne_at_PCP_edge',  'Longitude', 'Ne', 'PCP_flag', 'Radius', 'Timestamp', 'Latitude', 'MLT']
-data_list = ['Ne', 'PCP_flag', 'Timestamp', 'MLT', 'MLAT', 'Background_Ne']
+data_list = ['Ne', 'PCP_flag', 'Timestamp', 'MLT', 'MLAT', 'Foreground_Ne']
 sat_list = ['A', 'C']
 
 #path mac
@@ -66,13 +66,15 @@ for sat in sat_list:
 x_range = np.linspace(0, 100, 100001)
 
 
-def PCP_std(PCP, MLT, MLT_max, MLT_min, MLAT, Ne, Time, sat, Background, PCP_flag, PCP_proper):
+def PCP_std(PCP, MLT, MLT_max, MLT_min, MLAT, Ne, Time, sat, Foreground, PCP_flag, PCP_proper):
     ncount = 0    
     std_trail = 0
     std_lead = 0
     std_prop = 0
     a_count = 0
     b_count = 0
+    
+
     for i in trange(0, len(PCP), 2, desc = F'Calculating the standard deviation for satellite {sat}'):
         #, desc = F'Finding and plotting PCP for satellite {sat}'
         if (9 <= MLT_max <= 15) & (9 <= MLT_min <= 15):
@@ -102,8 +104,6 @@ def PCP_std(PCP, MLT, MLT_max, MLT_min, MLAT, Ne, Time, sat, Background, PCP_fla
                 end_prop = PCP_proper[np.argmin(np.abs(PCP_proper - end))]
 
 
-        # & (MLT_max >= MLT[start] >= MLT_min) & (MLT_min <= MLT[end] <= MLT_max)
-        # & (np.all(( MLT_max >= MLT[start:end] >= MLT_min)) == True)
         
         if (np.all((MLAT[start:end] > 0)) == True) & (np.all((Ne[start:end] > 0)) == True) & \
                                                      (MLT_max >= MLT[start] >= MLT_min) & \
@@ -124,14 +124,35 @@ def PCP_std(PCP, MLT, MLT_max, MLT_min, MLAT, Ne, Time, sat, Background, PCP_fla
             #y2 = np.interp(x_range, np.linspace(0, 100, len(Ne[start:end])), Ne[start:end])
             slope, intercept, r_value, p_value, std_err = stats.linregress(MLAT[start:start_prop], Ne[start:start_prop])
             slope1, intercept1, r_value1, p_value1, std_err1 = stats.linregress(MLAT[end_prop:end], Ne[end_prop:end])
-            if (np.isnan(std_err) == False) & (np.isnan(std_err1) == False):
-                a = std_err
-                b = std_err1
-                a_count += 1
-                b_count += 1
-            # c = np.std(Ne[start_prop:end_prop])
-                std_trail += a
-                std_lead += b
+            
+            slope3, intercept3, r_value3, p_value3, std_err2 = stats.linregress(MLAT[start:start_prop], Foreground[start:start_prop])
+            slope4, intercept4, r_value4, p_value4, std_err3 = stats.linregress(MLAT[end_prop:end], Foreground[end_prop:end])
+
+            if (np.isnan(std_err) == False) & (np.isnan(std_err1) == False) & (np.isnan(std_err2) == False) & (np.isnan(std_err3) == False):
+                f_Ne = std_err / std_err1
+                f_Foreground = std_err2 / std_err3
+
+                if (12 >= MLT[start] >= 9) & (9 <= MLT[end] <= 12):
+                    f_list_Ne_9_12.append(f_Ne)
+                    f_list_Fg_9_12.append(f_Foreground)
+                elif (15 >= MLT[start] >= 12) & (12 <= MLT[end] <= 15):
+                    f_list_Ne_12_15.append(f_Ne)
+                    f_list_Fg_12_15.append(f_Foreground)
+                elif (24 >= MLT[start] >= 21) & (21 <= MLT[end] <= 24):
+                    f_list_Ne_21_24.append(f_Ne)
+                    f_list_Fg_21_24.append(f_Foreground)
+                elif (3 >= MLT[start] >= 0) & (0 <= MLT[end] <= 3):
+                    f_list_Ne_0_03.append(f_Ne)
+                    f_list_Fg_0_03.append(f_Foreground)
+                
+
+            #     c = std_err2
+            #     d = std_err3
+            #     a_count += 1
+            #     b_count += 1
+            # # c = np.std(Ne[start_prop:end_prop])
+            #     std_trail += a
+            #     std_lead += b
             # std_prop += c
             # print('########')
             # print('start:', start)
@@ -155,11 +176,14 @@ def PCP_std(PCP, MLT, MLT_max, MLT_min, MLAT, Ne, Time, sat, Background, PCP_fla
     
         # if ncount == 100:
         #     break
-    print('##################################')
-    print(f'std_trail: {std_trail}')
-    print(f'std_lead: {std_lead}')
-    print(f'std_prop: {std_prop}')
-    print(f'Sum Ratio trail / lead Sat {sat} \n MLT: {MLT_min} - {MLT_max} :', std_trail / std_lead)
+    # print('##################################')
+    # print(f'std_trail: {std_trail}')
+    # print(f'std_lead: {std_lead}')
+    # print(f'std_prop: {std_prop}')
+    # print(f'Sum Ratio trail / lead Sat {sat} \n MLT: {MLT_min} - {MLT_max} :', std_trail / std_lead)
+    # print('######################################################')
+    # print(f_list_Ne_9_12)
+    # print('######################################################')
             
         
 # for sat in sat_list:
@@ -171,13 +195,36 @@ def PCP_std(PCP, MLT, MLT_max, MLT_min, MLAT, Ne, Time, sat, Background, PCP_fla
 
 MLT_max_list = np.array([12, 15, 24, 3])
 MLT_min_list = np.array([9, 12, 21, 0])
+f_list_Ne_9_12 = []
+f_list_Fg_9_12 = []
 
+f_list_Ne_12_15 = []
+f_list_Fg_12_15 = []
+
+f_list_Ne_21_24 = []
+f_list_Fg_21_24 = []
+
+f_list_Ne_0_03 = []
+f_list_Fg_0_03 = []
 for sat in sat_list:
     for i in range(len(MLT_max_list)):
         MLT_max = MLT_max_list[i]
         MLT_min = MLT_min_list[i]
-        exec(f'PCP_std(PCP_index_{sat}, MLT_{sat}, MLT_max, MLT_min, MLAT_{sat}, Ne_{sat}, Timestamp_{sat}, sat, Background_Ne_{sat}, PCP_flag_{sat}, PCP_proper_{sat})')
+        exec(f'PCP_std(PCP_index_{sat}, MLT_{sat}, MLT_max, MLT_min, MLAT_{sat}, Ne_{sat}, Timestamp_{sat}, sat, Foreground_Ne_{sat}, PCP_flag_{sat}, PCP_proper_{sat})')
         plt.close()
+
+print(f'### Currently saving the f ratio data... ###')
+np.save(f"C:/Users/krisfau/Desktop/VSCode/Data/Data_fratio_NH_Ne_9_12", np.array(f_list_Ne_9_12))
+np.save(f"C:/Users/krisfau/Desktop/VSCode/Data/Data_fratio_NH_Fg_9_12", np.array(f_list_Ne_9_12))
+
+np.save(f"C:/Users/krisfau/Desktop/VSCode/Data/Data_fratio_NH_Ne_12_15", np.array(f_list_Ne_12_15))
+np.save(f"C:/Users/krisfau/Desktop/VSCode/Data/Data_fratio_NH_Fg_12_15", np.array(f_list_Ne_12_15))
+
+np.save(f"C:/Users/krisfau/Desktop/VSCode/Data/Data_fratio_NH_Ne_21_24", np.array(f_list_Ne_21_24))
+np.save(f"C:/Users/krisfau/Desktop/VSCode/Data/Data_fratio_NH_Fg_21_24", np.array(f_list_Ne_21_24))
+
+np.save(f"C:/Users/krisfau/Desktop/VSCode/Data/Data_fratio_NH_Ne_0_03", np.array(f_list_Ne_0_03))
+np.save(f"C:/Users/krisfau/Desktop/VSCode/Data/Data_fratio_NH_Fg_0_03", np.array(f_list_Ne_0_03))
 
 
 
