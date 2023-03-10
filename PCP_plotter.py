37827ca0511e194e2cd2305ea9a2a17c962abcfa
Kristoffer Falk Austnes <kfaustnes@gmail.com>
2023-03-10 12:53:31 +0100

    Update PCP_plotter.py

--- PCP_plotter.py
+++ PCP_plotter.py
@@ -25,7 +25,7 @@ for sat in sat_list:
     PCP_indices = np.concatenate((change01, change10))
     exec(f'PCP_index_{sat} = np.sort(PCP_indices)')
 
-
+# DO I NEED TO PLOT WHERE THE TRAILING AND LEADING EDGE 
 # for sat in sat_list:
 #     exec(f'change14 = np.where((PCP_flag_{sat} == 4) & (PCP_flag_{sat} != np.roll(PCP_flag_{sat},1)))[0]')
 #     exec(f'change41 = np.where((PCP_flag_{sat} == 4) & (PCP_flag_{sat} != np.roll(PCP_flag_{sat},-1)))[0]')
@@ -35,43 +35,33 @@ for sat in sat_list:
 
 
 def PCP_plotter(PCP, MLT, MLT_max, MLT_min, MLAT, Ne, Time, sat, Background, PCP_flag):
-    ncount = 0    
-    scount = 0
+    PCP_count = 0    
+ 
 
     for i in trange(0, len(PCP), 2, desc = F'Finding and plotting PCP for satellite {sat}'):
-        #, desc = 'Finding and plotting PCP'
 
+        #Setting the start and end of every patch on the dayside
         if (9 <= MLT_max <= 15) & (9 <= MLT_min <= 15):
             if np.abs(MLAT[PCP[i]]) > np.abs(MLAT[PCP[i+1]]):
                 start = PCP[i+1]
                 end = PCP[i]
-
             elif np.abs(MLAT[PCP[i]]) < np.abs(MLAT[PCP[i+1]]):
                 start = PCP[i]
                 end = PCP[i+1]
-                
+
+        #Setting the start and end of every patch on the nightside
         elif ((21 <= MLT_max <= 24) & (21 <= MLT_min <= 24)) | (( 0 <= MLT_max <= 3) & (0 <= MLT_min <= 3)):
             if np.abs(MLAT[PCP[i]]) < np.abs(MLAT[PCP[i+1]]):
                 start = PCP[i+1]
                 end = PCP[i]
-
             elif np.abs(MLAT[PCP[i]]) > np.abs(MLAT[PCP[i+1]]):
                 start = PCP[i]
                 end = PCP[i+1]
         
-        # if np.abs(MLAT[PCP[i]]) > np.abs(MLAT[PCP[i+1]]):
-        #     start = PCP[i+1]
-        #     end = PCP[i]
-
-        # elif np.abs(MLAT[PCP[i]]) < np.abs(MLAT[PCP[i+1]]):
-        #     start = PCP[i]
-        #     end = PCP[i+1]
-        
+        #Range used in interpolating the PCP dataset
         x_range = np.linspace(0, 100, 100001)
 
-        # & (MLT_max >= MLT[start] >= MLT_min) & (MLT_min <= MLT[end] <= MLT_max)
-        # & (np.all(( MLT_max >= MLT[start:end] >= MLT_min)) == True)
-
+        #PCP requirements
         if (np.all((MLAT[start:end] > 0)) == True) & (np.all((Ne[start:end] > 0)) == True) & \
                                                      (MLT_max >= MLT[start] >= MLT_min) & \
                                                      (MLT_min <= MLT[end] <= MLT_max) & \
@@ -80,49 +70,26 @@ def PCP_plotter(PCP, MLT, MLT_max, MLT_min, MLAT, Ne, Time, sat, Background, PCP
                                                      (np.isnan(MLT[start]) == False) & \
                                                      (np.isnan(MLT[end]) == False) & \
                                                      (np.any((PCP_flag[start:end] == 4))):
-            ncount +=1
-            # print('#######')
-            # print('start:', start)
-            # print('end:', end)
-            # print('lengde:', np.abs(start-end))
-            # print('count:', ncount)
-            # print('Ne start:', Ne[start])
-            # print('Ne end:', Ne[end])
-            # print('MLAT start:', MLAT[start])
-            # print('MLAT end:', MLAT[end])
+            PCP_count += 1
 
+            #Interpolating so that the PCPs have the same length
             y1 = np.interp(x_range, np.linspace(0, 100, len(MLAT[start:end])), Ne[start:end])
 
             
-            #plt.xlabel('MLAT')
+            # plt.xlabel('MLAT')
             plt.ylabel('Ne ($10^4 cm^{-3}$)')
-            # plt.plot(MLAT[start:end], Ne[start:end] / 1e4, label = f'SWARM {sat}. PCP #{ncount}')
-            # plt.plot(MLAT[start:end], Background[start:end] / 1e4, label = f'SWARM {sat}. Background. PCP #{ncount}')
-            plt.plot(x_range, y1 /1e4, label = f'SWARM {sat}. Patch number: {ncount}')
-            # plt.annotate(xy =(MLAT[start+5], Ne[start+5]), text = 'PCP start.')
-            # plt.annotate(xy = (MLAT[end-5], Ne[end-5]), text = 'PCP end.')
-            # plt.vlines(MLAT[start], linestyles = 'dotted', label = 'PCP start', color = 'black')
-            # plt.vlines(MLAT[end], linestyles = 'dotted', label = 'PCP end', color = 'black')
-            # plt.yscale('log')
+            plt.plot(x_range, y1 /1e4, label = f'SWARM {sat}. Patch number: {PCP_count}')
 
 
-            if ncount % 5 == 0:
-                plt.title(f'Five Polar Cap Patches Northern Hemisphere, SWARM {sat}. \n For MLT {MLT_min}-{MLT_max}')
-                plt.legend(loc = 'lower center', fontsize = 5)
-                plt.savefig(f'NH_SWARM_{sat}_MLT{MLT_max}_{MLT_min}_PCP{ncount}.png')
+            #Plotting five PCPs on the same plot
+            if PCP_count % 5 == 0:
+                plt.title(f'Five Polar Cap Patches Northern Hemisphere, SWARM {sat}. \n MLT {MLT_min}-{MLT_max}')
+                plt.legend(loc = 'lower center', fontsize = 8)
+                plt.savefig(f'NH_SWARM_{sat}_MLT{MLT_max}_{MLT_min}_PCP{PCP_count}.png')
                 plt.close()
-        # if ncount == 20:
-        #     break
             
-            
-        
-# for sat in sat_list:
-#     exec(f'PCP_plotter(PCP_index_{sat}, MLT_{sat}, MLAT_{sat}, Ne_{sat}, Timestamp_{sat}, sat)')
 
-#PCP_plotter(PCP, MLT, MLT_max, MLT_min MLAT, Ne, Time, sat)
-
-#Input variables for plotting
-
+#Magnetic local time used for plotting the PCPs
 MLT_max_list = np.array([12, 15, 24, 3])
 MLT_min_list = np.array([9, 12, 21, 0])
 
