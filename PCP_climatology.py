b58487154eb04f3d23c08a2f8152ed7268c25f84
Kristoffer Falk Austnes <kfaustnes@gmail.com>
2023-03-29 01:36:04 +0200

    Update PCP_climatology.py

--- PCP_climatology.py
+++ PCP_climatology.py
@@ -72,3 +72,89 @@ for indicator in hemisphere_indicator:
     exec(f'Data_fratio_{indicator}_Ne_18_21 = np.load("{path}Data_fratio_{indicator}_Ne_18_21.npy", allow_pickle = True)')
     exec(f'Data_fratio_{indicator}_Fg_18_21 = np.load("{path}Data_fratio_{indicator}_Fg_18_21.npy", allow_pickle = True)')
     exec(f'Data_fratio_{indicator}_array_index_18_21 = np.load("{path}Data_fratio_{indicator}_array_index_18_21.npy", allow_pickle = True)')
+
+N_9_12 = Data_fratio_NH_Ne_9_12
+S_9_12 = Data_fratio_SH_Ne_9_12
+N_index_9_12 = Data_fratio_NH_array_index_9_12
+S_index_9_12 = Data_fratio_SH_array_index_9_12
+
+N_12_15 = Data_fratio_NH_Ne_12_15
+S_12_15 = Data_fratio_SH_Ne_12_15
+N_index_12_15 = Data_fratio_NH_array_index_12_15
+S_index_12_15 = Data_fratio_SH_array_index_12_15
+
+N_21_24 = Data_fratio_NH_Ne_21_24
+S_21_24 = Data_fratio_SH_Ne_21_24
+N_index_21_24 = Data_fratio_NH_array_index_21_24
+S_index_21_24 = Data_fratio_SH_array_index_21_24
+
+N_0_03 = Data_fratio_NH_Ne_0_03
+S_0_03 = Data_fratio_SH_Ne_0_03
+N_index_0_03 = Data_fratio_NH_array_index_0_03
+S_index_0_03 = Data_fratio_SH_array_index_0_03
+
+N_3_6 = Data_fratio_NH_Ne_3_6
+S_3_6 = Data_fratio_SH_Ne_3_6
+N_index_3_6 = Data_fratio_NH_array_index_3_6
+S_index_3_6 = Data_fratio_SH_array_index_3_6
+
+N_6_9 = Data_fratio_NH_Ne_6_9
+S_6_9 = Data_fratio_SH_Ne_6_9
+N_index_6_9 = Data_fratio_NH_array_index_6_9
+S_index_6_9 = Data_fratio_SH_array_index_6_9
+
+N_15_18 = Data_fratio_NH_Ne_15_18
+S_15_18 = Data_fratio_SH_Ne_15_18
+N_index_15_18 = Data_fratio_NH_array_index_15_18
+S_index_15_18 = Data_fratio_SH_array_index_15_18
+
+N_18_21 = Data_fratio_NH_Ne_18_21
+S_18_21 = Data_fratio_SH_Ne_18_21
+N_index_18_21 = Data_fratio_NH_array_index_18_21
+S_index_18_21 = Data_fratio_SH_array_index_18_21
+
+#Function to find where the indices starts to indicate data for satellite C and not A 
+# - this is used to indicate where the timestamp data needs to be changed
+def find_index(array):
+    index = -1
+    for i in range(1, len(array)):
+        if array[i] < array[i-1]:
+            index = i 
+            break 
+    return index
+
+
+def Hour_date():
+    pass 
+
+def PCP_climatology(index_array):
+    max_date = datetime.datetime = (2020, 1, 1)
+    PCP_index_list_A = []
+    PCP_index_list_C = []
+
+    #Satellite A
+    for i in range(0, len(index_array), 2):
+        if i <= find_index(index_array) - 1:
+            PCP_index_A = int((index_array[i+1] + index_array[i]) / 2)
+            PCP_index_list_A.append(PCP_index_A)
+        elif i > find_index(index_array) - 1:
+            PCP_index_C = int((index_array[i+1] + index_array[i]) / 2)
+            PCP_index_list_C.append(PCP_index_C)
+
+
+
+
+
+    print(len(PCP_index_list_A))
+    print(len(PCP_index_list_C))
+    return PCP_index_list_A, PCP_index_list_C
+#        while (Timestamp_A[i] < max_date) == True:
+#            for k in range(0, len(index_array), 2):
+#                print(k)
+#     
+PCP_climatology(N_index_9_12)
+
+print(N_index_9_12[630:640])
+print(N_index_9_12[:10])
+
+#         # plot andres timeseries of polar cap patches for winter and summer.
